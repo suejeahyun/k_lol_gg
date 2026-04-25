da2c63a5fd903c662e5d3644e1923737254d5cd2
suejeahyun <tjdmswo11@gmail.com>
2026-04-25 22:26:37 +0900

    feat: connect progress routes to sidebars

--- src/components/UserSidebar.tsx
+++ src/components/UserSidebar.tsx
@@ -3,35 +3,39 @@ import Link from "next/link";
 export default function UserSidebar() {
   return (
     <aside className="app-sidebar">
-      <div className="app-sidebar__title">유저 메뉴</div>
+      <div className="app-sidebar__title">메뉴</div>
 
       <nav className="app-sidebar__nav">
         <Link href="/" className="app-sidebar__link">
-          메인페이지
+          홈
         </Link>
+
         <Link href="/players" className="app-sidebar__link">
-          플레이어 목록 / 검색
+          플레이어
         </Link>
+
         <Link href="/matches" className="app-sidebar__link">
-          내전 목록 / 검색
+          내전 기록
         </Link>
-        <Link href="/players/balance" className="app-sidebar__link">
-          팀 밸런스
-        </Link>
+
         <Link href="/rankings" className="app-sidebar__link">
           랭킹
         </Link>
+
         <Link href="/progress" className="app-sidebar__link">
           진행현황
         </Link>
+
         <Link href="/progress/event" className="app-sidebar__link">
           이벤트 내전
         </Link>
+
+        <Link href="/progress/destruction" className="app-sidebar__link">
+          멸망전
+        </Link>
+
         <Link href="/notices" className="app-sidebar__link">
           공지사항
-        </Link>
-        <Link href="/images" className="app-sidebar__link">
-          멸망전 우승팀
         </Link>
       </nav>
     </aside>
