482a03587a4b5458197f6016e343e8f679013a38
suejeahyun <tjdmswo11@gmail.com>
2026-06-08 04:38:23 +0900

    내전등록 자동화

--- next.config.ts
+++ next.config.ts
@@ -26,12 +26,29 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  serverExternalPackages: ["tesseract.js", "tesseract.js-core"],
+  serverExternalPackages: [
+    "tesseract.js",
+    "tesseract.js-core",
+    "bmp-js",
+    "idb-keyval",
+    "is-url",
+    "node-fetch",
+    "regenerator-runtime",
+    "wasm-feature-detect",
+    "zlibjs",
+  ],
 
   outputFileTracingIncludes: {
     "/api/matches/import-lol-result": [
       "./node_modules/tesseract.js/**/*",
       "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/idb-keyval/**/*",
+      "./node_modules/is-url/**/*",
+      "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
+      "./node_modules/zlibjs/**/*",
     ],
   },
 };
